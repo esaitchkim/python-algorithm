4bcd7f5658f4f5d5070f7a6f85f6964b04a0b3ff
esaitchkim <esaitchkim@gmail.com>
2023-07-15 14:35:12 +0900

    add: 27889 (특별한 학교 이름)

---
+++ baekjoon/27000/27889.py
@@ -0,0 +1,11 @@
+"""27889.py
+특별한 학교 이름
+https://www.acmicpc.net/problem/27889
+"""
+
+school_abbr = {'NLCS':'North London Collegiate School',
+               'BHA':'Branksome Hall Asia',
+               'KIS':'Korea International School',
+               'SJA':'St. Johnsbury Academy'}
+
+print(school_abbr[input()])
