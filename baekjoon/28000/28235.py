6c89433c83b6cb930f45be307d3584815b0b0f27
esaitchkim <esaitchkim@gmail.com>
2023-07-15 14:24:53 +0900

    add: 28235 (코드마스터 2023)

---
+++ baekjoon/28000/28235.py
@@ -0,0 +1,11 @@
+"""28235.py
+코드마스터 2023
+https://www.acmicpc.net/problem/28235
+"""
+
+cheer_map = {'SONGDO':'HIGHSCHOOL',
+             'CODE':'MASTER',
+             '2023':'0611',
+             'ALGORITHM':'CONTEST'}
+
+print(cheer_map[input()])
