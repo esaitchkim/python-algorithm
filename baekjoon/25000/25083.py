3b67983ee63921063d69a55837af8c4440ffed99
devkimsangho <esaitchkim@gmail.com>
2023-07-15 12:44:24 +0900

    add: 25083 (새싹)

---
+++ baekjoon/25000/25083.py
@@ -0,0 +1,12 @@
+"""25083.py
+새싹
+https://www.acmicpc.net/problem/25083
+"""
+
+
+print('         ,r\'"7')
+print('r`-_   ,\'  ,/')
+print(' \\. ". L_r\'')
+print('   `~\\/')
+print('      |')
+print('      |')
